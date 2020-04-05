349d8777b0a15cf2f19f47f19c00d7cf2eb0c875
RoksanaBo <roksana85@hotmail.com>
2020-04-05 11:53:37 -0600

    first commit

---
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -0,0 +1,23 @@
+package com.weborders.pages;
+// it meant to be extended
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractBasePage {
+
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+
+    public AbstractBasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+        // for find By annotations.
+        // this code will be executed immediately when some object will be created
+
+    }
+
+
+}
