d2c92f4eeb806e131c57d618f283b1cc905b86c6
RoksanaBo <roksana85@hotmail.com>
2020-04-05 14:19:54 -0600

    Added Test Base class

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -3,6 +3,8 @@ package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,6 +12,15 @@ public abstract class AbstractBasePage {
 
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+
+    @FindBy(tagName = "h1")
+    protected WebElement pageLogo;
+
+
+    public String getPageLogoText(){
+        return pageLogo.getText();
+    }
 
 
     public AbstractBasePage(){
